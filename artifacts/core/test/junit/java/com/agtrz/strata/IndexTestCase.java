aace4f2617bd46b41d7813fa61009622b659e2f4
Alan Gutierrez <alan@blogometer.com>
2006-12-18 06:56:16 +0000

    git-svn-id: http://blogometer.com/svn/open/trunk/strata@1566 3df9ba58-24f1-0310-ade5-ba95bd924a04

--- artifacts/core/test/junit/java/com/agtrz/strata/IndexTestCase.java
+++ artifacts/core/test/junit/java/com/agtrz/strata/IndexTestCase.java
@@ -3,20 +3,10 @@
  */
 package com.agtrz.strata;
 
-import java.io.File;
-import java.io.IOException;
-import java.net.URI;
 import java.util.Arrays;
 import java.util.Comparator;
 
 import junit.framework.TestCase;
-
-import com.agtrz.sheaf.Sheaf;
-import com.agtrz.sheaf.SheafBuilder;
-import com.agtrz.sheaf.SheafFactory;
-import com.agtrz.sheaf.SheafLocker;
-import com.agtrz.sheaf.core.AppendJournal;
-import com.agtrz.strata.hash.HashIndexFactory;
 
 /**
  * @author Alan Gutierrez
@@ -24,9 +14,6 @@ import com.agtrz.strata.hash.HashIndexFactory;
 public class IndexTestCase
 extends TestCase
 {
-    private final static URI INDEX_URI = URI.create("http://agtrz.com/sheaf/2005/08/12/index");
-    private final static URI JOURNAL_URI = URI.create("http://agtrz.com/sheaf/2005/08/10/journal");
-
     private final static String[] ALPHABET = new String[]
     {                                        
         "alpha",
@@ -70,43 +57,6 @@ extends TestCase
         }
     }
 
-    public void _testCreateIndex()
-    throws IOException
-    {
-        File file = File.createTempFile("strata", ".st");
-        file.deleteOnExit();
-        
-        SheafBuilder newSheaf = SheafFactory.INSTANCE.newSheafBuilder();
-        
-        newSheaf.addJournal(JOURNAL_URI, AppendJournal.class);
-        
-        IndexFactory factory = HashIndexFactory.INSTANCE;
-        
-        
-        factory.newIndexCreator()
-               .create(INDEX_URI, newSheaf, StringComparator.class);
-        
-        Sheaf sheaf = newSheaf.newSheafCreator(256).create(file);
-        SheafLocker locker = sheaf.newSheafLocker(JOURNAL_URI);
-        
-        Index index = factory.newIndexOpener().open(INDEX_URI, locker);
-        
-        assertEquals(0, index.getCount());
-        assertEquals(StringComparator.class, index.getComparatorClass());
-
-        sheaf.close();
-        
-        sheaf = SheafFactory.INSTANCE.open(file);
-        locker = sheaf.newSheafLocker(JOURNAL_URI);
-      
-        index = factory.newIndexOpener().open(INDEX_URI, locker);
-        
-        assertEquals(0, index.getCount());
-        assertEquals(StringComparator.class, index.getComparatorClass());
-        
-        sheaf.close();
-    }
-    
     private final static class Split
     {
         public final char character;
