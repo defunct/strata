196a6c71acce1b162132364efbd39e3a10a89d81
Alan Gutierrez <alan@blogometer.com>
2008-08-11 04:24:54 +0000

    - Deleting non-generic Fossil and Strata.
    git-svn-id: http://blogometer.com/svn/open/trunk/strata@2281 3df9ba58-24f1-0310-ade5-ba95bd924a04

--- core/src/test/java/com/agtrz/strata/Extractor.java
+++
@@ -1,9 +0,0 @@
-/* Copyright Alan Gutierrez 2006 */
-package com.agtrz.strata;
-
-public interface Extractor<O, E extends Comparable<? super E>>
-{
-    E extract(O o);
-}
-
-/* vim: set et sw=4 ts=4 ai tw=78 nowrap: */
