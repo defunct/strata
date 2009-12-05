a2b17317a3470f8b148e5c6ce38573f93c2ba09c
Alan Gutierrez <alan@blogometer.com>
2009-12-05 05:29:11 -0600

    Mix build.

---
+++ src/mix/java/com/goodworkalan/strata/mix/StrataProject.java
@@ -0,0 +1,30 @@
+package com.goodworkalan.strata.mix;
+
+import com.goodworkalan.go.go.Artifact;
+import com.goodworkalan.mix.ProjectModule;
+import com.goodworkalan.mix.builder.Builder;
+import com.goodworkalan.mix.builder.JavaProject;
+
+public class StrataProject extends ProjectModule {
+    @Override
+    public void build(Builder builder) {
+        builder
+            .cookbook(JavaProject.class)
+                .produces(new Artifact("com.goodworkalan/strata/0.1"))
+                .main()
+                    .depends()
+                        .artifact(new Artifact("com.goodworkalan/stash/0.1"))
+                        .artifact(new Artifact("com.mallardsoft/tuple-partial/0.1.0"))
+                        .end()
+                    .end()
+                .test()
+                    .depends()
+                        .artifact(new Artifact("org.testng/testng/5.10/jdk15"))
+                        .artifact(new Artifact("args4j/args4j/2.0.8"))
+                        .artifact(new Artifact("org.mockito/mockito-core/1.6"))
+                        .end()
+                    .end()
+                .end()
+            .end();
+    }
+}
