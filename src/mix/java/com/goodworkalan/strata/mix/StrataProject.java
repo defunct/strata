8729a1da2160817b0e63b03672c3c4dda2210fef
Alan Gutierrez <alan@blogometer.com>
2010-06-01 22:26:55 -0500

    Mix ProjectModule is now an interface.

--- src/mix/java/com/goodworkalan/strata/mix/StrataProject.java
+++ src/mix/java/com/goodworkalan/strata/mix/StrataProject.java
@@ -4,24 +4,28 @@ import com.goodworkalan.mix.ProjectModule;
 import com.goodworkalan.mix.builder.Builder;
 import com.goodworkalan.mix.builder.JavaProject;
 
-public class StrataProject extends ProjectModule {
-    @Override
+/**
+ * Builds the project definition for Strata.
+ *
+ * @author Alan Gutierrez
+ */
+public class StrataProject implements ProjectModule {
+    /**
+     * Build the project definition for Strata.
+     *
+     * @param builder
+     *          The project builder.
+     */
     public void build(Builder builder) {
         builder
             .cookbook(JavaProject.class)
                 .produces("com.github.bigeasy.strata/strata/0.1")
-                .main()
-                    .depends()
-                        .include("com.github.bigeasy.stash/stash/0.1")
-                        .include("com.mallardsoft/tuple-partial/0.1.0")
-                        .end()
-                    .end()
-                .test()
-                    .depends()
-                        .include("org.testng/testng-jdk15/5.10")
-                        .include("args4j/args4j/2.0.8")
-                        .include("org.mockito/mockito-core/1.6")
-                        .end()
+                .depends()
+                    .production("com.github.bigeasy.stash/stash/0.1")
+                    .production("com.mallardsoft/tuple-partial/0.1.0")
+                    .development("org.testng/testng-jdk15/5.10")
+                    .development("args4j/args4j/2.0.8")
+                    .development("org.mockito/mockito-core/1.6")
                     .end()
                 .end()
             .end();
