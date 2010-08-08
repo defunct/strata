e0fd492e20ba47a635b296152a0d2f91a34c4b1e
Alan Gutierrez <alan@blogometer.com>
2010-08-08 08:37:55 -0500

    Removed Tuple.

--- src/cafe/java/com/goodworkalan/strata/StrataProject.java
+++ src/cafe/java/com/goodworkalan/strata/StrataProject.java
@@ -22,7 +22,6 @@ public class StrataProject implements ProjectModule {
                 .produces("com.github.bigeasy.strata/strata/0.1.0.1")
                 .depends()
                     .production("com.github.bigeasy.stash/stash/0.1")
-                    .production("com.mallardsoft/tuple-partial/0.1.0")
                     .development("org.testng/testng-jdk15/5.10")
                     .development("args4j/args4j/2.0.8")
                     .development("org.mockito/mockito-core/1.6")
